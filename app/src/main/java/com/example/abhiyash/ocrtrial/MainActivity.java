5aa7e932591cd01a990223761b3b42610bc364ce
Abhiyash <abhiyashm@gmail.com>
2017-12-01 19:49:28 +0530

    this version includes

--- app/src/main/java/com/example/abhiyash/ocrtrial/MainActivity.java
+++ app/src/main/java/com/example/abhiyash/ocrtrial/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.abhiyash.ocrtrial;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,25 +29,29 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView OCRTextView;
-    Bitmap image; //our image
-    private TessBaseAPI mTess; //Tess API reference
-    String datapath = "";
-    Button b1;
-    String OCRresult = null;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+        TextView OCRTextView;
+        Bitmap image; //our image
+        private TessBaseAPI mTess; //Tess API reference
+        String datapath = "";
+        Button b1;
+        String OCRresult = null;
+        private static int RESULT_LOAD_IMG = 1;
+        String imgDecodableString;
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1= (Button)findViewById(R.id.button);
+        b1 = (Button) findViewById(R.id.button);
 
         //init image
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.ani);
+        //image = BitmapFactory.decodeResource(getResources(),R.drawable.ani);
 
         //initialize Tesseract API
         String language = "eng";
-        datapath = getFilesDir()+ "/tesseract/";
+        datapath = getFilesDir() + "/tesseract/";
         mTess = new TessBaseAPI();
 
         checkFile(new File(datapath + "tessdata/"));
@@ -56,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         try {
             //location we want the file to be at
             String filepath = datapath + "/tessdata/eng.traineddata";
-            Log.d("12345",filepath);
+            Log.d("12345", filepath);
             //get access to AssetManager
             AssetManager assetManager = getAssets();
 
@@ -83,12 +92,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void checkFile(File dir) {
         //directory does not exist, but we can successfully create it
-        if (!dir.exists()&& dir.mkdirs()){
+        if (!dir.exists() && dir.mkdirs()) {
             copyFiles();
         }
         //The directory exists, but there is no data file in it
-        if(dir.exists()) {
-            String datafilepath = datapath+ "tessdata/eng.traineddata";
+        if (dir.exists()) {
+            String datafilepath = datapath + "tessdata/eng.traineddata";
             File datafile = new File(datafilepath);
             if (!datafile.exists()) {
                 copyFiles();
@@ -96,14 +105,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void processImage(View view){
+    public void processImage(View view) {
 
         mTess.setImage(image);
         OCRresult = mTess.getUTF8Text().toString();
 
         OCRTextView = (TextView) findViewById(R.id.OCRTextView);
         //EditText e=(EditText)findViewById(R.id.editText);
-        Toast.makeText(this, "Ocr ans="+OCRresult, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Ocr ans=" + OCRresult, Toast.LENGTH_SHORT).show();
         OCRTextView.setText(OCRresult);
         //e.setText("asd"+OCRresult);
     }
@@ -111,14 +120,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         //if(b1.isSelected()){
-            try {
-                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                //String term = editTextInput.getText().toString();
-                intent.putExtra(SearchManager.QUERY, OCRTextView.getText().toString());
-                startActivity(intent);
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
+        try {
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            //String term = editTextInput.getText().toString();
+            intent.putExtra(SearchManager.QUERY, OCRTextView.getText().toString());
+            startActivity(intent);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         //}
     }
-}
+
+    public void loadImagefromGallery(View view) {
+        // Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        // Create intent to Open Image applications like Gallery, Google Photos
+        System.out.println("1");
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        // Start the Intent
+        System.out.println("2");
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+        System.out.println("3");
+    }
+
+    @Override
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.out.println("4");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            System.out.println("5");
+            // When an Image is picked
+
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK && null != data) {
+                System.out.println("6");
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                System.out.println("7");
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                System.out.println("8");
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                System.out.println("9");
+                // Move to first row
+
+                cursor.moveToFirst();
+                System.out.println("10");
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                System.out.println("11");
+                imgDecodableString = cursor.getString(columnIndex);
+                System.out.println("12");
+                cursor.close();
+                System.out.println("13");
+                ImageView imgView = (ImageView) findViewById(R.id.imageView);
+                System.out.println("14");
+                // Set the Image in ImageView after decoding the String
+                imgView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                imgView.setImageBitmap(convertImageViewToBitmap(imgView));
+                System.out.println("15");
+
+            } else {
+
+                Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong" + e, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    public Bitmap convertImageViewToBitmap(ImageView v) {
+
+        image = ((BitmapDrawable) v.getDrawable()).getBitmap();
+        return image;
+    }
+
+        }
